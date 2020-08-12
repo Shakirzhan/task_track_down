d841a98be8ceee0bd211741047e4c6cfd7a00c4f
Shakirzhan <samarbaev99@mail.ru>
2020-08-12 20:48:06 +0300

    check version

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 var express = require('express');
+var soap = require('strong-soap').soap;
+
+
+
+// wsdl of the web service this client is going to invoke. For local wsdl you can use, url = './wsdls/stockquote.wsdl'
+var url = 'https://tracking.russianpost.ru/rtm34?wsdl';
+
+var requestArgs = {
+  symbol: 'IBM'
+};
+
+var options = {
+    'trace': 1,
+    'soap_version': 2
+  };
+
 
 var app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-  res.render('list');
+app.get('/:barcode', function(req, res) {
+   
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+
+    //EJ000385249RU
+    if(req.params.barcode) {
+        soap.createClient(url, options, function(err, client) {
+            client.getOperationHistory({
+                OperationHistoryRequest: { Barcode: req.params.barcode, MessageType: '0',Language: 'RUS' },
+                AuthorizationHeader: { login: 'ptKXgCyjzWMInE',password: 'KOgUrtw94459'}
+            }).then(response => {
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(JSON.stringify(response.result.OperationHistoryData)));
+            });
+        });
+    }
+
 })
 
 app.listen(3000);
